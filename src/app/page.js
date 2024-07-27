17180ac7c74428a88af4912e9e561584ebf95c89
gabriel <contato.gabrielfeitosa@gmail.com>
2024-07-27 17:09:25 -0500

    value reset on home page

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,26 @@ import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { Title } from "@/components/Title";
 import { SelectPayment } from "@/components/SelectPayment";
+import { useContext, useEffect } from "react";
+import { InstallmentContext } from "@/contexts/installmentContext";
 
 export default function Home() {
+  const {
+    setAmount,
+    setFirstPayment,
+    setRestPayment,
+    setCashPayment,
+    setFirstPaymentDone,
+  } = useContext(InstallmentContext);
+
+  useEffect(() => {
+    setAmount(0);
+    setFirstPayment(0);
+    setRestPayment(0);
+    setCashPayment(false);
+    setFirstPaymentDone(false);
+  }, []);
+
   return (
     <>
       <Header />
