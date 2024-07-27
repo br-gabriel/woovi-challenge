4bbcd6951379ca9314c353eb35a4fb34b6b2a26d
gabriel <contato.gabrielfeitosa@gmail.com>
2024-07-26 20:59:32 -0500

    footer component created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import LogoFooter from "../../public/LogoFooter.svg";
+import { GoShieldCheck } from "react-icons/go";
+
+export function Footer() {
+  return (
+    <footer className="w-full flex items-center justify-center gap-1 text-[#b2b2b2] font-semibold text-sm my-8">
+      <GoShieldCheck size={19} />
+      Pagamento 100% seguro via:
+      <Image src={LogoFooter} width={60} alt="logo da woovi" className="mb-1" />
+    </footer>
+  );
+}
