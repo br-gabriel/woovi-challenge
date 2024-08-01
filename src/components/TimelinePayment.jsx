b5501a7578c62df00de9bebd151fefc793b0dd39
gabriel <contato.gabrielfeitosa@gmail.com>
2024-08-01 01:29:18 -0500

    Removed unnecessary divs

--- src/components/TimelinePayment.jsx
+++ src/components/TimelinePayment.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { Stack, Box, Radio, useTheme } from "@mui/material";
+import { Stack, Radio, useTheme } from "@mui/material";
 import { TimelineConnector } from "@mui/lab";
 import { FaCircleCheck } from "react-icons/fa6";
 import { useContext } from "react";
@@ -11,50 +11,46 @@ export function TimelinePayment() {
 
   return (
     <Stack className="mt-2">
-      <Box>
-        <Stack>
+      <Radio
+        enable
+        checked={firstPaymentDone == true ? true : false}
+        checkedIcon={<FaCircleCheck size={17} />}
+        sx={{
+          "& .MuiSvgIcon-root": {
+            color: theme.palette.primary.main,
+            fontSize: 19,
+          },
+          padding: 0,
+        }}
+      />
+
+      {!cashPayment && (
+        <Stack sx={{ alignItems: "center" }}>
+          <TimelineConnector
+            sx={{
+              height: "1.5rem",
+              mt: "-0.20rem",
+              backgroundColor: theme.palette.primary.light,
+            }}
+          />
           <Radio
             enable
-            checked={firstPaymentDone == true ? true : false}
-            checkedIcon={<FaCircleCheck size={17} />}
+            checked={false}
+            checkedIcon={<FaCircleCheck />}
             sx={{
               "& .MuiSvgIcon-root": {
                 color: theme.palette.primary.main,
                 fontSize: 19,
+                mt: "-0.125rem",
+                fill: firstPaymentDone
+                  ? theme.palette.primary.main
+                  : theme.palette.primary.light,
               },
               padding: 0,
             }}
           />
-
-          {!cashPayment && (
-            <Stack sx={{ alignItems: "center" }}>
-              <TimelineConnector
-                sx={{
-                  height: "1.5rem",
-                  mt: "-0.20rem",
-                  backgroundColor: theme.palette.primary.light,
-                }}
-              />
-              <Radio
-                enable
-                checked={false}
-                checkedIcon={<FaCircleCheck />}
-                sx={{
-                  "& .MuiSvgIcon-root": {
-                    color: theme.palette.primary.main,
-                    fontSize: 19,
-                    mt: "-0.125rem",
-                    fill: firstPaymentDone
-                      ? theme.palette.primary.main
-                      : theme.palette.primary.light,
-                  },
-                  padding: 0,
-                }}
-              />
-            </Stack>
-          )}
         </Stack>
-      </Box>
+      )}
     </Stack>
   );
 }
